9a476cc498cbc1f227adad002d782f55ff68a2b2
Jessica Martin <jessica.martin@messagesystems.com>
2018-06-20 16:49:29 -0400

    added twilio support and also added readme

---
+++ README.md
@@ -0,0 +1,18 @@
+# Installation
+```
+pip install sparkpost
+pip install twilio
+```
+
+# Configuration
+```
+export SPKEY='your sparkpost api key here'
+export TWILIOKEY='your twilio api key here'
+export TWILIOACCT='your twilio account sid here'
+```
+
+# Running
+```
+chmod +x detect.py
+./detect.py
+```
--- detect.py
+++ detect.py
@@ -2,25 +2,44 @@
 
 import RPi.GPIO as GPIO
 from sparkpost import SparkPost
+from twilio.rest import twilioClient
 import time
 import os
 
+# Connection Setup for SP and Twilio
 sparkpostKey = os.environ.get('SPKEY')
 twilioKey = os.environ.get('TWILIOKEY')
+twilioAccount = os.environ.get('TWILIOACCT')
 sparky = SparkPost(sparkpostKey)
+twilio = twilioClient(twilioAccount, twilioKey)
+
 
 # Yay, you don't have water anymore
 def sendHappyEmail():
-    sparkySays = sparky.transmissions.send(
+    sparky.transmissions.send(
         recipients=['jessica.martin@sparkpost.com'],
         template='detector-email-happy'
     )
 
+def sendHappyText():
+    twilio.messages.create(
+        to='+14436059355',
+        from_='+14438927539',
+        body='Hooray! No more water in your basement'
+    )
+
 # Boo, you have water detected
 def sendSadEmail():
-    sparkySays = sparky.transmissions.send(
+    sparky.transmissions.send(
         recipients=['jessica.martin@sparkpost.com'],
         template='detector-email-sad'
+    )
+
+def sendSadText():
+    twilio.messages.create(
+        to='+14436059355',
+        from_='+14438927539',
+        body='Uh oh! We have detected that there is water in your basement'
     )
 
 # Generic callback handler for any state change
@@ -28,16 +47,18 @@ def inputCallback(channel):
     if GPIO.input(channel):
         print('No Water Detected')
         sendHappyEmail()
+        sendHappyText()
     else:
         print('Water Detected')
         sendSadEmail()
+        sendSadText()
 
 # Setup the pin we are listening to
 GPIO.setmode(GPIO.BOARD)
 channel = 38
 GPIO.setup(channel, GPIO.IN)
 
-# Add our event handlers and callback (bouncetime is for preventing false positive changes
+# Add our event handlers and callback (bouncetime is for preventing false positive changes)
 GPIO.add_event_detect(channel, GPIO.BOTH, bouncetime=1000)
 GPIO.add_event_callback(channel, inputCallback)
 
